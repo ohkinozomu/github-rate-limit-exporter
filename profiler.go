ce020163ba8fb3d62878521934863d60d87329fe
Nozomu Ohki <38576286+ohkinozomu@users.noreply.github.com>
2022-03-02 05:40:32 +0900

    Add cloud_profiler (#61)

--- profiler.go
+++ profiler.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"context"
+	"errors"
 	"os"
 
 	ncp "github.com/ohkinozomu/neutral-cp"
 )
 
 func StartProfiler() error {
+	var registry ncp.Registry
+	if os.Getenv("PROFILER_REGISTRY") == "pyroscope" {
+		registry = ncp.PYROSCOPE
+	} else if os.Getenv("PROFILER_REGISTRY") == "cloud_profiler" {
+		registry = ncp.CLOUD_PROFILER
+	} else {
+		return errors.New("undefined profiler registry")
+	}
+
 	c := ncp.Config{
-		Registry:        ncp.PYROSCOPE,
+		Registry:        registry,
 		ApplicationName: "github-rate-limit-exporter",
 		ServerAddress:   os.Getenv("SERVER_ADDRESS"),
+		Version:         "0.0.8",
 	}
 	ncp := ncp.NeutralCP{Config: c}
 
